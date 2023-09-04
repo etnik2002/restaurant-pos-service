f57cacd6e46a13589e51b2b3e24038fc9f0923f0
etnik2002 <etnikz2002@gmail.com>
2023-09-04 21:51:42 +0200

    added routes

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
-const { createOrder, getOrderById } = require("../controllers/order-controller");
+const { createOrder, getOrderById, payOrder, prepareOrder } = require("../controllers/order-controller");
 
 
 router.post('/create/:restaurant_id/:table_id', createOrder);
 
 router.get('/:id', getOrderById);
+
+router.post('/pay/:id', payOrder)
+
+router.post('/prepare/:id', prepareOrder)
 
 module.exports = router;
