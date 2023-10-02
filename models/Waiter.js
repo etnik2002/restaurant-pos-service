6ed11511f23d33149eb2a14431028d92a7cb3f90
etnik zeqiri <etnikz2002@gmail.com>
2023-10-02 17:31:40 +0200

    waiter roles

--- models/Waiter.js
+++ models/Waiter.js
@@ -4,14 +4,13 @@ const jwt = require("jsonwebtoken");
 const waiterSchema = mongoose.Schema({
     name: { type: String },
     pin: { type: Number },
+    role: { type: String },
     restaurant_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' },
 })
 
 waiterSchema.methods.generateAuthToken = function (data) {
     data.password = undefined;
-    const token = jwt.sign({ data }, process.env.OUR_SECRET, {
-        expiresIn: '365d',
-    });
+    const token = jwt.sign({ data }, process.env.OUR_SECRET);
 
     return token;
 };
