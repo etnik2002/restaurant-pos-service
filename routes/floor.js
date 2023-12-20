24e95c84491b2a12564d40df9a8caa982f46952f
Etnik Zeqiri <46058633+etnik2002@users.noreply.github.com>
2023-12-20 14:57:45 +0100

    Update floor.js

--- routes/floor.js
+++ routes/floor.js
@@ -9,6 +9,6 @@ router.post('/:id',deleteFloor);
 
 router.get('/all', getAllFloors)
 
-router.get('/tables/:restaurant_id/:floor_id',cache('10 minutes'), getFloorTables);
+router.get('/tables/:restaurant_id/:floor_id', getFloorTables);
 
 module.exports = router;
